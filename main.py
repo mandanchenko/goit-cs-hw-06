30ea2613ab853c404fc38e9031e31bdeb358423d
Andanchenko Maria <andanchenko.maria@gmail.com>
2024-03-23 02:30:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from multiprocessing import Process
 
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -16,8 +15,9 @@ import logging
 URI = "mongodb://mongodb:27017"
 
 
-SOCKET_HOST = '127.0.0.1'
+SOCKET_HOST = "127.0.0.1"
 SOCKET_PORT = 80
+
 
 def send_data_to_socket(data):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,12 +25,13 @@ def send_data_to_socket(data):
     sock.sendto(data, server)
     sock.close()
 
+
 class HttpHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        data = self.rfile.read(int(self.headers['Content-Length']))
+        data = self.rfile.read(int(self.headers["Content-Length"]))
         send_data_to_socket(data)
         self.send_response(200)
-        self.send_header('Location', '/')
+        self.send_header("Location", "/")
         self.end_headers()
 
     def do_GET(self):
@@ -62,12 +63,14 @@ class HttpHandler(BaseHTTPRequestHandler):
         else:
             self.send_header("Content-type", "text/plain")
         self.end_headers()
-        with open(f'.{self.path}', "rb") as f:
+        with open(f".{self.path}", "rb") as f:
             self.wfile.write(f.read())
+
+
 def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
-    server_address = ('0.0.0.0', 3000)
+    server_address = ("0.0.0.0", 3000)
     httpd = server_class(server_address, handler_class)
-    logging.info(f'starting server: {server_address}\n')
+    logging.info(f"starting server: {server_address}\n")
 
     try:
         httpd.serve_forever()
@@ -75,18 +78,22 @@ def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
         logging.error(e)
         httpd.server_close()
 
+
 def save_data(data):
-    client = MongoClient(URI, server_api=ServerApi('1'))
+    client = MongoClient(URI, server_api=ServerApi("1"))
     db = client.final_project
     data_parse = urllib.parse.unquote_plus(data.decode())
     try:
-        data_parse = {key: value for key, value in [el.split("=") for el in data_parse.split("&")]}
-        data_parse['date'] = str(datetime.now())
+        data_parse = {
+            key: value for key, value in [el.split("=") for el in data_parse.split("&")]
+        }
+        data_parse["date"] = str(datetime.now())
         db.messages.insert_one(data_parse)
     except Exception as e:
         logging.error(e)
     finally:
         client.close()
+
 
 def run_socket_server(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -100,6 +107,7 @@ def run_socket_server(ip, port):
         logging.error(e)
     finally:
         sock.close()
+
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s")
