badfd85024f356d503f85fc7528bf6de90dc7ac4
Andanchenko Maria <andanchenko.maria@gmail.com>
2024-03-23 02:03:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from multiprocessing import Process
+
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from pathlib import Path
 
 
-import json
 import mimetypes
 import urllib.parse
 import pathlib
 import socket
 import logging
 
-uri = "mongodb://mongodb:27017"
+URI = "mongodb://mongodb:27017"
+
 
 SOCKET_HOST = '127.0.0.1'
-SOCKET_PORT = 4000
-
-BASE_DIR=Path(__file__).parent
+SOCKET_PORT = 80
 
 def send_data_to_socket(data):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,18 +27,87 @@ def send_data_to_socket(data):
 
 class HttpHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        pass
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        send_data_to_socket(data)
+        self.send_response(200)
+        self.send_header('Location', '/')
+        self.end_headers()
 
     def do_GET(self):
-        router = urllib.urlparse(self.path).path
+        router = urllib.parse.urlparse(self.path).path
         match router:
             case "/":
                 self.send_html("index.html")
             case "/message":
                 self.send_html("message.html")
             case _:
-                file = BASE_DIR.joinpath(router[1:])
+                file = pathlib.Path(router[1:])
                 if file.exists():
                     self.send_static(file)
                 else:
                     self.send_html("error.html", 404)
+
+    def send_html(self, filename, status=200):
+        self.send_response(int(status))
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        with open(filename, "rb") as f:
+            self.wfile.write(f.read())
+
+    def send_static(self, status=200):
+        self.send_response(int(status))
+        mimetype = mimetypes.guess_type(self.path)
+        if mimetype:
+            self.send_header("Content-type", mimetype[0])
+        else:
+            self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        with open(f'.{self.path}', "rb") as f:
+            self.wfile.write(f.read())
+def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ('0.0.0.0', 3000)
+    httpd = server_class(server_address, handler_class)
+    logging.info(f'starting server: {server_address}\n')
+
+    try:
+        httpd.serve_forever()
+    except Exception as e:
+        logging.error(e)
+        httpd.server_close()
+
+def save_data(data):
+    client = MongoClient(URI, server_api=ServerApi('1'))
+    db = client.final_project
+    data_parse = urllib.parse.unquote_plus(data.decode())
+    try:
+        data_parse = {key: value for key, value in [el.split("=") for el in data_parse.split("&")]}
+        data_parse['date'] = str(datetime.now())
+        db.messages.insert_one(data_parse)
+    except Exception as e:
+        logging.error(e)
+    finally:
+        client.close()
+
+def run_socket_server(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    sock.bind(server)
+    try:
+        while True:
+            data, addr = sock.recvfrom(1024)
+            save_data(data)
+    except Exception as e:
+        logging.error(e)
+    finally:
+        sock.close()
+
+if __name__ == "__main__":
+    logging.basicConfig(format="%(levelname)s: %(message)s")
+
+    pr_server = Process(target=run_http_server, args=(HTTPServer, HttpHandler))
+    pr_server.start()
+    pr_socket = Process(target=run_socket_server, args=(SOCKET_HOST, SOCKET_PORT))
+    pr_socket.start()
+
+    pr_server.join()
+    pr_socket.join()
